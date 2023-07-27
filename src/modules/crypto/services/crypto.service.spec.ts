7dc5e5151f1578ae10edce7850b698b41a1fa469
Rahino Quijano <rahinoquijano@gmail.com>
2023-07-27 23:27:53 +0800

    refactor: extract cryptographic features into a new module

--- src/modules/crypto/services/crypto.service.spec.ts
+++ src/modules/crypto/services/crypto.service.spec.ts
@@ -1,15 +1,15 @@
 import { Test, TestingModule } from '@nestjs/testing';
-import { HashService } from './hash.service';
+import { CryptoService } from './crypto.service';
 
-describe('HashService', () => {
-  let service: HashService;
+describe('CryptoService', () => {
+  let service: CryptoService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [HashService],
+      providers: [CryptoService],
     }).compile();
 
-    service = module.get<HashService>(HashService);
+    service = module.get<CryptoService>(CryptoService);
   });
 
   it('should be defined', () => {
