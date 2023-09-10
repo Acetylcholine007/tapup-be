a76eac81427faae8330bff3a49781194ea9dbbbb
Rahino Quijano <rahinoquijano@gmail.com>
2023-09-10 11:40:50 +0800

    refactor: enhance health check implementation

--- src/modules/health/controllers/health.controller.ts
+++ src/modules/health/controllers/health.controller.ts
@@ -1,13 +1,26 @@
 import { Public } from '@common/decorators/public.decorator';
 import { Controller, Get } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
+import {
+  HealthCheck,
+  HealthCheckService,
+  TypeOrmHealthIndicator,
+} from '@nestjs/terminus';
 
+@ApiTags('Health Check')
 @Controller('health')
 export class HealthController {
+  constructor(
+    private readonly healthCheckService: HealthCheckService,
+    private readonly typeOrmHealthIndicator: TypeOrmHealthIndicator
+  ) {}
+
   @Public()
   @Get()
+  @HealthCheck()
   check() {
-    return {
-      ok: true,
-    };
+    return this.healthCheckService.check([
+      () => this.typeOrmHealthIndicator.pingCheck('database'),
+    ]);
   }
 }
